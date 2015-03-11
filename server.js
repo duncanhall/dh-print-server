4539a4529dbcb23d6fac7b3bbe749e4f02192bcf
Duncan Hall <himself@duncanhall.net>
2015-03-11 22:52:20 +0000

    Basic browser -> server -> printer

---
+++ server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var path = require('path');
+var sockets = require(path.resolve('app/sockets'));
+
+
+app.use('/', express.static(__dirname + '/public'));
+
+sockets.init(http);
+
+var server = http.listen(1985, function () {
+
+  console.log('Printatron listening on 1985');
+
+});
